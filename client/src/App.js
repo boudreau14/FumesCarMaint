eb6c09f09e0d4403c2b8807a9e6de78dabe32854
dfuenning <dfuenning@gmail.com>
2020-07-11 10:28:25 -0400

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
-      <p className="App-intro">
+      <p className="App-intro stuff">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
