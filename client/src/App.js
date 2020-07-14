bb69da75d2d654541ab4e57df7fe8da1421f1eac
boudreau14 <60114894+boudreau14@users.noreply.github.com>
2020-07-13 21:31:27 -0400

    Basic nav, working on passport backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from './components/Navbar';
+import Wrapper from "./components/Wrapper";
+import Register from "./pages/Register";
+import Main from "./pages/Main";
 
 //Bryan is our master!
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/register" component={Register} />
+        </Wrapper>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
