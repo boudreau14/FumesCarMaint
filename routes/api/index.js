c23375bb58a19f4b3f10344051700467cbc08844
boudreau14 <60114894+boudreau14@users.noreply.github.com>
2020-07-18 12:07:04 -0400

    Added Services to db

--- routes/api/index.js
+++ routes/api/index.js
@@ -23,4 +23,24 @@ router.get("/logout", function (req, res) {
     res.json({message: "You are logged out!"})
 });
 
+//SERVICES ROUTES
+router.get("/services", (req, res) => {
+  return res.json(data.services);
+});
+
+router.post("/services", (req, res) => {
+  let services = [], id = null;
+  let cart = JSON.parse(req.body.cart);
+  if (!cart) return res.json(services)
+  for (var i=0; i < data.services.length; i++) {
+    id = data.services[i].id.toString();
+    if (cart.hasOwnProperty(id)) {
+      data.services[i].qty = cart[id]
+      services.push(data.services[i]);
+    }
+  }
+  return res.json(services);
+} )
+
+
 module.exports = router;
